40d71edfb6a0700d78278c1757de1b1c84708fe1
SimonReilich <simon.reilich@proton.me>
2026-02-13 23:09:04 +0100

    added dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component, viewChild } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { NgtCanvas, NgtCanvasContent } from 'angular-three/dom';
 import { FluidScene } from './components/fluid-gradient.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgtCanvas, NgtCanvasContent, FluidScene], 
+  imports: [NgtCanvas, NgtCanvasContent, FluidScene],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    isDarkMode = false;
+
+    constructor(private cdr: ChangeDetectorRef) {}
+
+    ngOnInit() {
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        this.isDarkMode = mediaQuery.matches;
+        console.log(this.isDarkMode);
+        mediaQuery.addEventListener('change', (e) => {
+            this.isDarkMode = e.matches;
+            console.log(this.isDarkMode);
+            this.cdr.detectChanges();
+        });
+    }
+}
